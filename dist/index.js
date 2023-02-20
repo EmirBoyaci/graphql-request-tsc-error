14aedc00c8877906bbbd0a08d74b5e69f0901d8d
EmirBoyaci <emirboyaci@icloud.com>
2023-02-20 22:54:16 +0300

    initial commit

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.GraphQLClient = void 0;
+const graphql_request_1 = require("graphql-request");
+Object.defineProperty(exports, "GraphQLClient", { enumerable: true, get: function () { return graphql_request_1.GraphQLClient; } });
